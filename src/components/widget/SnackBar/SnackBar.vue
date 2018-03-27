8706ce79681bb94d98be558d7f689839784dfce2
thunderobot <137224350@qq.com>
2018-03-27 15:17:37 +0800

    一、SnackBar新增滑动关闭

--- src/components/widget/SnackBar/SnackBar.vue
+++ src/components/widget/SnackBar/SnackBar.vue
@@ -9,10 +9,16 @@
 <template>
     <div class="snackbar">
         <transition :name="directionData[direction]"
-                    v-on:after-leave="afterLeave"
-        >
-            <div class="snackbar-wrapper" v-show="isShow" :class="{bottom:position ===1}">
-                <div class="content border-1px">{{content}}</div>
+                    v-on:after-leave="afterLeave">
+            <div class="snackbar-wrapper" v-show="isShow" :class="{bottom:position ===1}"
+                 ref="snackbar"
+            >
+                <div class="content border-1px"
+                     @touchstart="touchstart"
+                     @touchmove="touchmove"
+                     @touchend="touchend"
+                >{{content}}
+                </div>
                 <div class="btn" @click="hide">{{btnText}}</div>
             </div>
         </transition>
@@ -20,6 +26,7 @@
 </template>
 <!--script-->
 <script type="text/ecmascript-6">
+    let startX, startY, moveX, moveY;
     export default {
         data() {
             return {
@@ -28,7 +35,8 @@
                 content: "", // 显示的文本
                 timer: undefined, // 计时器
                 callBack: undefined, // 回调
-                isClick: false
+                isClick: false,
+                snackbar: undefined
             }
         },
         created() {
@@ -36,6 +44,9 @@
             let width = document.documentElement.clientWidth || document.body.clientWidth;
             let htmlDom = document.getElementsByTagName("html")[0]
             htmlDom.style.fontSize = width / 10 + 'px';
+        },
+        mounted() {
+            this.snackbar = this.$refs.snackbar;
         },
         props: {
             position: {
@@ -52,7 +63,7 @@
             },
             displayDuration: {//显示时长，默认5秒
                 type: Number,
-                default: 5000
+                default: 4800
             }
         },
         methods: {
@@ -68,12 +79,11 @@
                 if (this.timer) {
                     clearTimeout(this.timer)
                 }
-                let that = this;
                 // 显示
-                that.isShow = true;
+                this.isShow = true;
                 // 5 秒后移除
-                this.timer = setTimeout(function () {
-                    that.isShow = false;
+                this.timer = setTimeout(() => {
+                    this.isShow = false;
                 }, this.displayDuration);
             },
             // 点击确定按钮，动画消失，如果有回调，消失后执行
@@ -86,6 +96,64 @@
             afterLeave() {
                 if (this.callBack && this.isClick)
                     this.callBack();
+            },
+            touchstart(e) {
+                // 移除掉定时器
+                if (this.timer)
+                    clearTimeout(this.timer)
+                startX = e.touches[0].pageX;
+                startY = e.touches[0].pageY;
+            },
+            touchmove(e) {
+                moveX = e.touches[0].pageX - startX;
+                moveY = e.touches[0].pageY - startY;
+                if (Math.abs(moveX) > Math.abs(moveY)) {
+                    // 水平移动
+                    this.snackbar.style.transition = `none`
+                    this.snackbar.style.transform = `translateX(${moveX}px)`;
+                    // 计算透明度
+                    let percent = Math.abs(moveX) / this.snackbar.getBoundingClientRect().width / 3;
+                    percent = (10 * percent).toFixed(1);
+                    percent = percent > .7 ? .7 : percent;
+                    percent = percent <= 0.3 ? 0.3 : percent;
+                    this.snackbar.style.opacity = 1 - percent;
+                }
+            },
+            touchend() {
+                // 没有到阈值 归位
+                if (Math.abs(moveX) < this.snackbar.getBoundingClientRect().width / 3) {
+                    this.snackbar.style.transition = `.3s`;
+                    this.snackbar.style.transform = `translateX(0)`;
+                    this.snackbar.style.opacity = 1;
+                    setTimeout(() => {
+                        this.snackbar.removeAttribute("style")
+                    }, 300)
+                    if (this.timer)
+                        clearTimeout(this.timer)
+                    this.timer = setTimeout(() => {
+                        this.isShow = false;
+                    }, this.displayDuration);
+                } else {
+                    // 隐藏掉吐司
+                    if (moveX > 0) { // 向右移动
+                        this.snackbar.style.transition = `.3s`;
+                        this.snackbar.style.transform = `translateX(100%)`;
+                    } else { // 向左移动
+                        this.snackbar.style.transition = `.3s`;
+                        this.snackbar.style.transform = `translateX(-100%)`;
+                    }
+                    setTimeout(() => {
+                        this.isShow = false;
+                        this.snackbar.style.display = "none";
+                        setTimeout(() => {
+                            this.snackbar.removeAttribute("style")
+                        }, 100)
+                    }, 300)
+                }
+                startY = 0;
+                startX = 0;
+                moveX = 0;
+                moveY = 0;
             }
         }
     }
@@ -119,8 +187,8 @@
             left 0
             display flex
             width 100%
-            height px2rem(50px)
-            background #373A41
+            height px2rem(48px)
+            background #393A3F
             transition all 0.5s
             z-index 2147483647
             &.bottom
@@ -153,8 +221,8 @@
                     transform translate3d(0, 100%, 0)
             .content, .btn
                 color #fff
-                height px2rem(50px)
-                line-height px2rem(50px)
+                height px2rem(48px)
+                line-height px2rem(48px)
                 font-size px2rem(14px)
                 user-select: none
                 -webkit-user-select none
